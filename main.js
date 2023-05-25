dad38538d24e1ca46311da867ad5a8d2a93c5589
Kenney Kong <kenneykong@gmail.com>
2023-05-25 17:25:17 -0400

    created readme.md

---
+++ main.js
@@ -0,0 +1,40 @@
+//NFL
+function showNFLfeed() {
+    const nflTwitter = document.getElementById('NFL-twit');
+    if (nflTwitter.style.display === "none") {
+      nflTwitter.style.display = "block";
+    } else {
+      nflTwitter.style.display = "none";
+    }
+  }
+  
+  //NBA
+  function showNBAfeed() {
+    const nbaTwitter = document.getElementById('NBA-twit');
+    if (nbaTwitter.style.display === "none") {
+      nbaTwitter.style.display = "block";
+    } else {
+      nbaTwitter.style.display = "none";
+    }
+  }
+  
+  //MLB
+  function showMLBfeed() {
+    const mlbTwitter = document.getElementById('MLB-twit');
+    if (mlbTwitter.style.display === "none") {
+      mlbTwitter.style.display = "block";
+    } else {
+      mlbTwitter.style.display = "none";
+    }
+  }
+  
+  //NHL
+  function showNHLfeed() {
+    const nhlTwitter = document.getElementById('NHL-twit');
+    if (nhlTwitter.style.display === "none") {
+      nhlTwitter.style.display = "block";
+    } else {
+      nhlTwitter.style.display = "none";
+    }
+  }
+  
